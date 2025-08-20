1ae177204a6966c5741c694f193d8ff52993f162
hassan5464 <hassanelmonge034@gmail.com>
2025-08-20 07:57:02 +0300

    I forget something

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <section className="h-screen bg-slate-600">
+
+      </section>
     </main>
   );
 }
