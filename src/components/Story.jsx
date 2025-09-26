d2f5e5bf42a7407099b2ce9d4e6327fa152d1252
hassan5464 <hassanelmonge034@gmail.com>
2025-09-26 19:51:24 +0300

    Implement story section

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react'
 import AnimatedTitle from './Animatedtitle'
 import gsap from 'gsap'
 import RoundedCorners from './RoundedCorners'
+import Button from './Button';
 
 const Story = () => {
   const frameRef = useRef(null)
@@ -70,6 +71,21 @@ const Story = () => {
               </div>
             </div>
             <RoundedCorners />
+            
+          </div>
+        </div>
+        <div className="flex -mt-80 w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start ">
+              where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5"
+            />
           </div>
         </div>
       </div> 
